ed4c341aaa3d9d97360d9d2148041e8c53225da1
J Faleiro <j@falei.ro>
2014-10-12 13:03:49 -0400

    legends are a pain

--- plot4.R
+++ plot4.R
@@ -13,8 +13,8 @@ with(d, {
     points(DateTime, Sub_metering_1, type='l', col='black')
     points(DateTime, Sub_metering_2, type='l', col='red')
     points(DateTime, Sub_metering_3, type='l', col='blue')
-    # adds legend, don't know a way to derive this automatically from points, so spelling it out...    
-    legend("topright", c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c('black', 'red', 'blue'), lwd=1, cex=0.2)
+    # adds legend, don't know a way to derive this automatically from points, so spelling it out... we gave precedence to the way the PNG is generated, not the screen
+    legend("topright", c('Sub_metering_1','Sub_metering_2','Sub_metering_3'), col=c('black', 'red', 'blue'), lwd=1, cex=0.5, text.width=strwidth("Sub_met     "))
     # plot 2,2
     plot(DateTime, Global_reactive_power, xlab='datetime', type='l') 
 })
